e749e2d965524ef4fb309ce8fe39d65401ba7791
DELGADO CASTRO <E032493@ad.bbva.com>
2026-02-22 18:07:21 +0100

    Localization and CMS setup: Add Netlify Identity email templates and configure passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("emails");
   eleventyConfig.addPassthroughCopy("admin");
 
   // Collections
